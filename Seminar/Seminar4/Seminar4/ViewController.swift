a2c5f08953bda0b6d5d22aac108cbf3ec8caa5ff
김태현 <taehyk528@gmail.com>
2022-05-06 15:52:02 +0900

    [#2] Alamofire 설치

---
+++ Seminar/Seminar4/Seminar4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar4
+//
+//  Created by taehy.k on 2022/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
